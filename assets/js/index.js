e539574d598d9de84785cd344b39e7c3e067ed74
Thomas Kelly <tmkelly28@gmail.com>
2017-09-06 12:31:02 -0400

    intial commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+import '../stylesheets/index.scss'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <h1>Hello</h1>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: './assets/js/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
