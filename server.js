1122e705daad3435ea96626f372c16a121963aba
Leah Gerald <Leanator13@gmail.com>
2018-01-21 18:24:00 -0500

    added deploy script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const server = app.listen(PORT, function() {
     console.log(`listening on port ${PORT}`)
